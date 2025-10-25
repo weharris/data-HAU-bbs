4986430a0af1a79363c1ab3f8da0dc2e463deaf5
W. Edwin Harris <weh9000@gmail.com>
2025-10-25 10:55:03 +0100

    Add exploratory data analysis (EDA) script for HAU BBS data with setup and analysis sections

--- scripts/EDA.R
+++ scripts/EDA.R
@@ -2,3 +2,16 @@
 # who: Ed H
 # what: EDA for HAU BBS data
 # when:2025-10-25
+
+# Contents ####
+# 1 Setup and data
+# 2 Analysis
+
+# 1 Setup and data ####
+library(readxl)
+
+mydat <- read_xlsx("data/birddata_tidy.xlsx")
+
+# 2 Analysis ####
+table(mydat$bird_spp)
+length(table(mydat$bird_spp))
