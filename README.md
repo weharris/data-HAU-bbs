fb207b442b738b4b9eb113dd60fce855cf2e1022
W. Edwin Harris <weh9000@gmail.com>
2025-10-25 13:25:49 +0100

    Enhance README.md to include detailed descriptions of data files and sources, and update EDA.R script with additional analysis and visualizations for HAU BBS data.

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 
 
-## Data sources
-https://outlook.office365.com/groups/live.harper.ac.uk/HAUBiodiversityData/files
+## 
 
-This is data from the Harper Adams University Breeding Bird Survey (HAU BBS). The sampling is inspired by the British Trust for Ornithology Breeding Bird Survey [(BTO BBS)(https://www.bto.org/get-involved/volunteer/projects/bbs)].
+## Data files and sources
+**birddata.csv** data/raw/ [Julia C. et al. HAU ES Wildlife Sharepoint](https://outlook.office365.com/groups/live.harper.ac.uk/HAUBiodiversityData/files) This is data from the Harper Adams University Breeding Bird Survey (HAU BBS). The sampling is inspired by the British Trust for Ornithology Breeding Bird Survey [(BTO BBS)(https://www.bto.org/get-involved/volunteer/projects/bbs)].
+
+**birddata_tidy.xlsx** data/ [Ed H. Tidy Data version of birddata.csv](https://github.com/weharris/data-HAU-bbs/tree/main/data)
+
+**Master_species_observation_list.xlsx** ./data/ [Megan L. consolidated HAU Biodiversity file](https://github.com/HAU-Biodiversity/shiny_biodiversity_dash/tree/main/data)
+
+
 
 ## Context data
 
--- scripts/EDA.R
+++ scripts/EDA.R
@@ -9,9 +9,60 @@
 
 # 1 Setup and data ####
 library(readxl)
+library(tidyverse)
+library(gt)
+library(ggplot2)
 
 mydat <- read_xlsx("data/birddata_tidy.xlsx")
 
 # 2 Analysis ####
-table(mydat$bird_spp)
+
+# Unique common names
+sort(unique(mydat$bird_spp))
+
+# how many observations total?
+nrow(mydat)
+
+# How many observations per year?
+for(i in 2022:2024){
+  print(c(i, length(mydat$year[mydat$year == i])))
+} 
+
+# How many unique species in total?
 length(table(mydat$bird_spp))
+
+# How many species per year?
+for(i in 2022:2024) {
+  print(length(table(mydat$bird_spp[mydat$year==i])))
+}
+
+# How many transects are there?
+length(unique(sort(mydat$transect)))
+
+# How many transect "walks" have there been?
+length(table(mydat$year, mydat$transect, mydat$occasion))
+
+# How many transect "walks" have there been?
+for(i in 2022:2024){
+  print(length(table(mydat$transect[mydat$year==i], mydat$occasion[mydat$year==i])))
+}
+
+par(mar = c(5, 10, 4, 2) + 0.1)
+barplot(sort(table(mydat$bird_spp)),
+        horiz=T,
+        las = 2,
+        cex.names = .5)
+par(mar(c(5, 4, 4, 2) + 0.1))
+
+mydat |>
+  count(bird_spp) |>
+  mutate(bird_spp = fct_reorder(bird_spp, n)) |> 
+  ggplot(aes(x = n, y = bird_spp)) +
+  geom_col(fill = "darkblue") +  
+  labs(
+    x = 'Counts',
+    y = element_blank(),
+    title = 'HAU Breeding Birds Survey 2022-2023'
+  ) +
+  theme_minimal(base_size = 10) 
+
